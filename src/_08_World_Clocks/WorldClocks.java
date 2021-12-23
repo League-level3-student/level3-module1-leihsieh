62d15248640a77d4405c5cc30a61a54b5edaba97
leihs <leihs@DESKTOP-D5U9TN8.lan>
2021-12-22 19:57:26 -0800

    Level 3 Mod 1

--- src/_08_World_Clocks/WorldClocks.java
+++ src/_08_World_Clocks/WorldClocks.java
@@ -76,8 +76,6 @@ public class WorldClocks implements ActionListener {
 		frame.add(panel);
 		panel.add(textArea);
 		panel.add(button);
-		//textArea.setText(city + "\n" + dateStr);
-		//textArea.setText(textArea.getText() + "\n" + "San Diego, US" + makeCity("San Diego, US"));
 
 		// This Timer object is set to call the actionPerformed() method every
 		// 1000 milliseconds
